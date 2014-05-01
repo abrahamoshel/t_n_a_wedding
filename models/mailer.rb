a67bf66d4904d0167e2ac683c428a55240d36c86
Abraham Oshel <abraham@bradsdeals.com>
2014-05-01 15:45:13 -0500

    adding basic setup for sinatra backend with email sending

---
+++ models/mailer.rb
@@ -0,0 +1,20 @@
+module Mailer
+  Mail.defaults do
+    delivery_method :smtp, { :address              => ENV["MAILER_SERVER"],
+                             :port                 => 587,
+                             :user_name            => ENV["MAILER_USER"],
+                             :password             => ENV["MAILER_PASS"],
+                             :authentication       => ENV["MAILER_AUTH"],
+                             :enable_starttls_auto => true  }
+  end
+
+  def self.deliver(from=nil, body=nil)
+    Mail.new do
+      from     "tiffanie.marie@gmail.com"
+      cc       ENV["MAILER_CC"]
+      to       ENV["MAILER_TO"]
+      subject  "New RSVP From #{from}"
+      body     body
+    end.deliver
+  end
+end
