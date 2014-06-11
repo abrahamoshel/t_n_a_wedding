8e21260d8da7173afc8600a503d0d1847cb44740
abrahamoshel <abraham.oshel@gmail.com>
2014-06-10 19:34:05 -0500

    sending emails

--- models/mailer.rb
+++ models/mailer.rb
@@ -1,11 +1,11 @@
 module Mailer
   Mail.defaults do
-    delivery_method :smtp, { :address              => ENV["MAILER_SERVER"],
-                             :port                 => 587,
-                             :user_name            => ENV["MAILER_USER"],
-                             :password             => ENV["MAILER_PASS"],
-                             :authentication => :plain,
-                             :enable_starttls_auto => true  }
+    delivery_method :smtp_connection, { :address              => ENV["MAILER_SERVER"],
+                                        :port                 => 587,
+                                        :user_name            => ENV["MAILER_USER"],
+                                        :password             => ENV["MAILER_PASS"],
+                                        :authentication       => :plain,
+                                        :enable_starttls_auto => true  }
   end
 
   def draft_email
